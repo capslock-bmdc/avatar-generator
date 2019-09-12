b02b24c0af7cee9ce0ff34e8d54f78e3186cb9d8
ZviKarp <zvi@zektec.com>
2019-09-12 22:24:08 +0300

    renamed template to background, removed old files

--- generator.py
+++ generator.py
@@ -3,11 +3,11 @@ import random
 import os
 
 path = 'images/png/'
-avatar = Image.open('images/template.png');
+avatar = Image.open('images/background.png');
 for dir in os.listdir(path):
     max_number = len(os.listdir(path+dir))
     random_number = str(random.randint(1,max_number))
-    part = Image.open(path+"/"+dir+"/"+dir+random_number+".png")
+    part = Image.open(path+dir+"/"+dir+random_number+".png")
     avatar = Image.alpha_composite(avatar, part)
 
 avatar.save("avatar.png")
