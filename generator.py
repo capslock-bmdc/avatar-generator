c6a18d233002facc8be14340e80f346e69114966
ZviKarp <zvi@zektec.com>
2019-09-16 13:01:10 +0300

    added images and edited their folder location

--- generator.py
+++ generator.py
@@ -1,13 +1,14 @@
 from PIL import Image
 import random
 import os
-
-path = 'images/png/'
-avatar = Image.open('images/background.png');
-for dir in os.listdir(path):
-    max_number = len(os.listdir(path+dir))
+images_path = 'images/png/'
+body_path = images_path + 'body/'
+background_path = images_path + 'background/'
+avatar = Image.open(background_path + '/background.png');
+for dir in os.listdir(body_path):
+    max_number = len(os.listdir(body_path+dir))
     random_number = str(random.randint(1,max_number))
-    part = Image.open(path+dir+"/"+dir+random_number+".png")
+    part = Image.open(body_path+dir+"/"+dir+random_number+".png")
     avatar = Image.alpha_composite(avatar, part)
 
 avatar.save("avatar.png")
