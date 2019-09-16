0897504032f98daec27e66eb1b86de4cc675e406
ZviKarp <zvi@zektec.com>
2019-09-16 13:05:43 +0300

    added antennas support :)

--- generator.py
+++ generator.py
@@ -1,14 +1,24 @@
 from PIL import Image
 import random
 import os
+
 images_path = 'images/png/'
-body_path = images_path + 'body/'
 background_path = images_path + 'background/'
-avatar = Image.open(background_path + '/background.png');
+body_path = images_path + 'body/'
+antennas_path = images_path + 'antennas/'
+antennas_category = '2antennas'
+
+avatar = Image.open(background_path + '/background.png')
+
 for dir in os.listdir(body_path):
     max_number = len(os.listdir(body_path+dir))
     random_number = str(random.randint(1,max_number))
     part = Image.open(body_path+dir+"/"+dir+random_number+".png")
     avatar = Image.alpha_composite(avatar, part)
+
+max_number = len(os.listdir(antennas_path+antennas_category))
+random_number = str(random.randint(1,max_number))
+part = Image.open(antennas_path+antennas_category+"/"+antennas_category+random_number+".png")
+avatar = Image.alpha_composite(avatar, part)
 
 avatar.save("avatar.png")
